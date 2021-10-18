59e11f28c48c8ac873ee76c6059513581663061a
gokoy <gokoy50@gmail.com>
2021-10-19 01:15:58 +0900

    refactor: CONSUMER, CEO 로그인 Role 체크

--- src/main/java/com/gokoy/delivery/global/error/exception/ErrorCode.java
+++ src/main/java/com/gokoy/delivery/global/error/exception/ErrorCode.java
@@ -1,11 +1,12 @@
 package com.gokoy.delivery.global.error.exception;
 
 public enum ErrorCode {
-	EMAIL_NOT_FOUND(404, "C001", "Email not found"),
-	EMAIL_EXISTS(400, "C002", "The email already exists"),
-	PWD_MISMATCH(400, "C003", "The passwords do not match"),
-	ARGUMENT_NOT_VALID(400, "C004", "The argument is not valid"),
-	NO_RESULT(404, "C005", "No results were found for your search")
+	EMAIL_NOT_FOUND(404, "C001", "Email not found."),
+	EMAIL_EXISTS(400, "C002", "The email already exists."),
+	PWD_MISMATCH(400, "C003", "The passwords do not match."),
+	ARGUMENT_NOT_VALID(400, "C004", "The argument is not valid."),
+	NO_RESULT(404, "C005", "No results were found for your search."),
+	INVALID_JWT(403, "C006", "Invalid JWT.")
 	;
 
 	private int status;
