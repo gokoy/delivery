9f459bb5584b49805e9b549edd3073fae6c5256d
gokoy <gokoy50@gmail.com>
2021-10-25 23:19:45 +0900

    test: DataJpaTest에서 QueryDsl 사용할 수 있도록 Test용 Configuration 설정

--- src/test/java/com/gokoy/delivery/domain/store/dao/StoreRepositoryTest.java
+++ src/test/java/com/gokoy/delivery/domain/store/dao/StoreRepositoryTest.java
@@ -1,28 +1,28 @@
 package com.gokoy.delivery.domain.store.dao;
 
+import com.gokoy.delivery.config.TestConfig;
 import com.gokoy.delivery.domain.store.domain.Category;
 import com.gokoy.delivery.domain.store.domain.Store;
 import com.gokoy.delivery.domain.store.domain.StoreType;
 import com.gokoy.delivery.global.common.model.Money;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+@DataJpaTest // 내부에 @ExtendWith(SpringExtension.class) 포함
+@Import(TestConfig.class)
 class StoreRepositoryTest {
 
     @Autowired
     private StoreRepository storeRepository;
 
     @Test
-    void 카테고리로_store_찾기() {
+    void QueryDsl_카테고리로_store_찾기() {
         //given
         Store store1 = Store.builder()
                 .name("store1")
