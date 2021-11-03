b801f02f387bb8c86323f91320ec7bf07d43cba6
gokoy <gokoy50@gmail.com>
2021-11-03 23:31:35 +0900

    refactor: swagger ignore parameter 설정 변경
    AuthenticationPrincipal 어노테이션 파라미터 ignore

--- src/main/java/com/gokoy/delivery/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/gokoy/delivery/global/config/swagger/SwaggerConfig.java
@@ -1,53 +1,51 @@
 package com.gokoy.delivery.global.config.swagger;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
+import com.gokoy.delivery.global.config.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.gokoy.delivery.global.config.security.JwtTokenProvider;
-
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.OperationContext;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
-	@Bean
-	public Docket swaggerApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.select()
-			.apis(RequestHandlerSelectors.any())
-			.paths(PathSelectors.any())
-			.build()
-			.securityContexts(Collections.singletonList(securityContext()))
-			.securitySchemes(Collections.singletonList(apiKey()));
-	}
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()));
+    }
 
-	private ApiKey apiKey() {
-		return new ApiKey("JWT", JwtTokenProvider.AUTH_HEADER, "header");
-	}
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", JwtTokenProvider.AUTH_HEADER, "header");
+    }
 
-	private SecurityContext securityContext() {
-		return SecurityContext
-			.builder()
-			.securityReferences(defaultAuth())
-			.operationSelector(operationContext -> true)
-			.build();
-	}
+    private SecurityContext securityContext() {
+        return SecurityContext
+                .builder()
+                .securityReferences(defaultAuth())
+                .operationSelector(operationContext -> true)
+                .build();
+    }
 
-	List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
-	}
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
+    }
 }
