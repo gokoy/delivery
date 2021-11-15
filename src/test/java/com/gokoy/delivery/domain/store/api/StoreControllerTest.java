e68e8a8b0167a108cb05242f55c29142c3d407a1
gokoy <gokoy50@gmail.com>
2021-11-15 21:50:44 +0900

    test

---
+++ src/test/java/com/gokoy/delivery/domain/store/api/StoreControllerTest.java
@@ -0,0 +1,58 @@
+package com.gokoy.delivery.domain.store.api;
+
+import com.gokoy.delivery.domain.store.application.StoreService;
+import com.gokoy.delivery.global.config.security.JwtAuthenticationFilter;
+import com.gokoy.delivery.global.config.security.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = StoreController.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                classes = {SecurityConfig.class, JwtAuthenticationFilter.class}))
+class StoreControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private StoreService storeService;
+
+    @Test
+    @WithMockUser(roles = "CEO")
+    void getSimpleStoresByCategory_유효성_검사() throws Exception {
+        //given
+        BDDMockito
+                .given(storeService.getSimpleStoresByCategory(any(String.class)))
+                .willReturn(null);
+
+        LinkedMultiValueMap<String, String> requestParam = new LinkedMultiValueMap<>();
+        requestParam.set("storeTypeRequest", null);
+
+        //when
+        ResultActions result = mvc.perform(get("/stores")
+                .params(requestParam)
+                .contentType(MediaType.APPLICATION_JSON));
+//                .andDo(print());
+
+        //then
+
+        result
+                .andExpect(status().is4xxClientError());
+
+    }
+
+}
---
+++ src/test/java/com/gokoy/delivery/domain/store/application/StoreServiceTest.java
@@ -0,0 +1,7 @@
+package com.gokoy.delivery.domain.store.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreServiceTest {
+
+}
