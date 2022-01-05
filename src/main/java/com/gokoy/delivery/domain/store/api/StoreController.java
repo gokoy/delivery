668798aaab51239b8489a48627b4e4bb784171d8
gokoy <gokoy50@gmail.com>
2022-01-05 23:15:49 +0900

    feat: Store 도메인 CRUD API 작성

--- src/main/java/com/gokoy/delivery/domain/store/api/StoreController.java
+++ src/main/java/com/gokoy/delivery/domain/store/api/StoreController.java
@@ -2,8 +2,10 @@ package com.gokoy.delivery.domain.store.api;
 
 import com.gokoy.delivery.domain.store.application.StoreService;
 import com.gokoy.delivery.domain.store.domain.StoreType;
-import com.gokoy.delivery.domain.store.dto.SimpleStoreResponse;
-import com.gokoy.delivery.domain.store.dto.StoreRequest;
+import com.gokoy.delivery.domain.store.dto.CreateStoreDto;
+import com.gokoy.delivery.domain.store.dto.SimpleStoreDto;
+import com.gokoy.delivery.domain.store.dto.StoreDto;
+import com.gokoy.delivery.domain.store.dto.UpdateStoreDto;
 import com.gokoy.delivery.global.common.response.SimpleResponse;
 import com.gokoy.delivery.global.config.validation.ValidEnum;
 import lombok.RequiredArgsConstructor;
@@ -23,24 +25,36 @@ public class StoreController {
 
     private final StoreService storeService;
 
-    @GetMapping
-    public List<SimpleStoreResponse> getSimpleStoresByCategory(@RequestParam
-                                                               @ValidEnum(enumClass = StoreType.class,
-                                                                       message = "Invalid value. Please enter the correct store type.")
-                                                                       String storeType) {
-        return storeService.getSimpleStoresByCategory(storeType.toUpperCase());
+    @PostMapping
+    public ResponseEntity<SimpleResponse> createStore(@AuthenticationPrincipal String userEmail,
+                                                      @Valid @RequestBody CreateStoreDto createStoreDto) {
+        return ResponseEntity.ok().body(storeService.createStore(userEmail, createStoreDto));
     }
 
-    @PostMapping
-    public ResponseEntity<SimpleResponse> createStore(@AuthenticationPrincipal String userEmail, @Valid @RequestBody StoreRequest storeRequest) {
-        return ResponseEntity.ok().body(storeService.createStore(userEmail, storeRequest));
+    @GetMapping("/{storeId}")
+    public ResponseEntity<StoreDto> readStore(@AuthenticationPrincipal String userEmail,
+                                              @PathVariable("storeId") Long storeId) {
+        return ResponseEntity.ok().body(storeService.readStore(userEmail, storeId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SimpleStoreDto>> readStoresByCategory(@ValidEnum(
+            enumClass = StoreType.class,
+            message = "Invalid value. Please enter the correct store type.") String storeType) {
+        return ResponseEntity.ok().body(storeService.readStoresByCategory(storeType));
     }
 
     @PutMapping("/{storeId}")
     public ResponseEntity<SimpleResponse> updateStore(@AuthenticationPrincipal String userEmail,
                                                       @PathVariable("storeId") Long storeId,
-                                                      @Valid @RequestBody StoreRequest storeRequest) {
-        return ResponseEntity.ok().body(storeService.updateStore(userEmail, storeId, storeRequest));
+                                                      @Valid @RequestBody UpdateStoreDto updateStoreDto) {
+        return ResponseEntity.ok().body(storeService.updateStore(userEmail, storeId, updateStoreDto));
+    }
+
+    @DeleteMapping("/{storeId}")
+    public ResponseEntity<SimpleResponse> deleteStore(@AuthenticationPrincipal String userEmail,
+                                                      @PathVariable("storeId") Long storeId) {
+        return ResponseEntity.ok().body(storeService.deleteStore(userEmail, storeId));
     }
 
 }
