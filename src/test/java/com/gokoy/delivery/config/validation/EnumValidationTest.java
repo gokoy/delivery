e0200e8da6ca4b303180e92834ad8d9747cd60d7
gokoy <gokoy50@gmail.com>
2022-01-05 23:11:58 +0900

    test: 커스텀 Validation 테스트 케이스 추가
    null 허용하지 않을 경우 테스트 추가

--- src/test/java/com/gokoy/delivery/config/validation/EnumValidationTest.java
+++ src/test/java/com/gokoy/delivery/config/validation/EnumValidationTest.java
@@ -5,6 +5,7 @@ import com.gokoy.delivery.global.config.validation.ValidEnum;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -34,7 +35,7 @@ public class EnumValidationTest {
     @ValueSource(strings = {"korean", "school_food", "dessert", "japanese",
             "chicken", "pizza", "asian_western", "chinese", "jokbal_bossam", "midnight_meal",
             "steam_soup", "lunch_box", "fast_food", "vegetarian"})
-    void Enum_Custom_Validator_성공_테스트(String param) {
+    void Enum_Custom_Validator_올바른_인자_테스트(String param) {
         //given
         EnumValidationTestClass testClass = new EnumValidationTestClass(param);
 
@@ -47,7 +48,7 @@ public class EnumValidationTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"", "a", "korea", "school_foodd"})
-    void Enum_Custom_Validator_실패_테스트(String param) {
+    void Enum_Custom_Validator_예외_발생_인자_테스트(String param) {
         //given
         EnumValidationTestClass testClass = new EnumValidationTestClass(param);
 
@@ -58,9 +59,21 @@ public class EnumValidationTest {
         Assertions.assertThat(validate).isNotEmpty();
     }
 
-    class EnumValidationTestClass {
+    @Test
+    void Enum_null_허용_테스트() {
+        //given
+        EnumValidationTestClass testClass = new EnumValidationTestClass(null);
+
+        //when
+        Set<ConstraintViolation<EnumValidationTestClass>> validate = validator.validate(testClass);
+
+        //then
+        Assertions.assertThat(validate).isNotEmpty();
+    }
+
+    static class EnumValidationTestClass {
         @ValidEnum(enumClass = StoreType.class)
-        private String storeType;
+        private final String storeType;
 
         public EnumValidationTestClass(String storeType) {
             this.storeType = storeType;
